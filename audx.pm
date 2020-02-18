adc5e61901b23dae6f0e25aef2b178a70ed172a0
Geoff Levand <geoff@infradead.org>
2020-02-17 19:17:36 -0800

    Change to MIT Plus license
    Signed-off-by: Geoff Levand <geoff@infradead.org>

--- audx.pm
+++ audx.pm
@@ -2,10 +2,6 @@
 #
 # Copyright 2008 Geoff Levand
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; version 2 of the License.
-#
 
 package audx;
 require Exporter;
--- mart.c
+++ mart.c
@@ -3,15 +3,6 @@
  *
  *  Copyright 2009 Geoff Levand
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License, Version 2 as
- *  published by the Free Software Foundation.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
  * Amazon product images:
  *  http://images.amazon.com/images/P/[ASIN].01.[FORMAT]
  *  ASIN = amazon shop id number
